946a986c47fd1d8a4ea4947e2ca9092215c9864a
Rizqon Maulana <rizqonmaulana5@gmail.com>
2021-10-21 23:53:56 +0700

    config for heroku

--- config/db.go
+++ config/db.go
@@ -3,26 +3,51 @@ package config
 import (
 	"fmt"
 	"go-mall/models"
+	"go-mall/utils"
+	"os"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+
 	"gorm.io/gorm"
 )
 
 func ConnectDataBase() *gorm.DB {
-	username := "root"
-	password := ""
-	host := "tcp(127.0.0.1:3306)"
-	database := "go_mall_db"
+	environment := utils.Getenv("ENVIRONMENT", "development")
 
-	dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+	if environment == "production" {
+		username := os.Getenv("DATABASE_USERNAME")
+		password := os.Getenv("DATABASE_PASSWORD")
+		host := os.Getenv("DATABASE_HOST")
+		port := os.Getenv("DATABASE_PORT")
+		database := os.Getenv("DATABASE_NAME")
+		// production
+		dsn := "host=" + host + " user=" + username + " password=" + password + " dbname=" + database + " port=" + port + " sslmode=require"
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic(err.Error())
+		}
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		db.AutoMigrate(&models.Category{}, &models.Order{}, &models.OrderDetail{}, &models.Product{}, &models.ProductRating{}, &models.Customer{}, &models.Seller{})
 
-	if err != nil {
-		panic(err.Error())
+		return db
+	} else {
+		// development
+		username := "root"
+		password := ""
+		host := "tcp(127.0.0.1:3306)"
+		database := "go_mall_db"
+
+		dsn := fmt.Sprintf("%v:%v@%v/%v?charset=utf8mb4&parseTime=True&loc=Local", username, password, host, database)
+		db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		db.AutoMigrate(&models.Category{}, &models.Order{}, &models.OrderDetail{}, &models.Product{}, &models.ProductRating{}, &models.Customer{}, &models.Seller{})
+
+		return db
 	}
 
-	db.AutoMigrate(&models.Category{}, &models.Order{}, &models.OrderDetail{}, &models.Product{}, &models.ProductRating{}, &models.Customer{}, &models.Seller{})
-
-	return db
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,6 @@
 module go-mall
 
+// +heroku goVersion go1.17
 go 1.17
 
 require (
@@ -20,6 +21,14 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.10.0 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
+	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/jackc/pgx/v4 v4.13.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
@@ -45,5 +54,6 @@ require (
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/mysql v1.1.2 // indirect
+	gorm.io/driver/postgres v1.1.2 // indirect
 	gorm.io/gorm v1.21.16 // indirect
 )
